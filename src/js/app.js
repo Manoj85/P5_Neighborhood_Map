e13f2c39fb7ab4d41f35d81a2dfc2ee9c74eaf25
Manoj Talagadadeevi <manojt.854@gmail.com>
2016-12-12 09:56:07 -0500

    Initial Layout

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+var ViewModel = function () {
+    "use strict";
+    let self = this;
+    this.appTitle = ko.observable("Neighborhood Insights");
+
+
+};
+
+ko.applyBindings(new ViewModel());
