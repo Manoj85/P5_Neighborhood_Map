8ae6fa873005b808333e0d0470099d4247014ae9
Manoj Talagadadeevi <manojt.854@gmail.com>
2016-12-12 16:55:52 -0500

    Load Google Map for a specific location

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Udacity - P5 Neighborhood Map Project
 
 
 https://manoj85.github.io/P5_Neighborhood_Map/ 
+
+
+##### References
+- http://www.latlong.net/
+- http://knockoutjs.com/documentation/
--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 
+// Global variables
+let map,
+    placeInfoWindow;
 
 
+// This function will be called on successful load of Google Maps API
+function mapApiLoadSuccess() {
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7413549, lng: -73.9980244},
+        zoom: 13
+    });
+}
 
-
+// Function called when there is an error in loading google maps
+function mapApiLoadError() {
+    "use strict";
+    console.log("Map Load Error! Please refresh the page.")
+}
 
 var ViewModel = function () {
     "use strict";
-    let self = this;
 
     // Setting the Application Title
     this.appTitle = ko.observable("Neighborhood Insights");
 
-
 };
 
+// Activating Knockout.js
 ko.applyBindings(new ViewModel());
