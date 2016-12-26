f5000ac0e091858844a7479e0e72e9f78f36d9be
Manoj Talagadadeevi <manojt.854@gmail.com>
2016-12-26 14:26:03 -0500

    Populate infowindow content for displayed markers

--- js/app.js
+++ js/app.js
@@ -2,32 +2,25 @@
 // Global variables
 let map,
     mapOptions,
-    markers = [],
     infowindow,
     bounds;
 const start_point = {lat: 40.7413549, lng: -73.9980244};  
 
-
 // Model for the Venue
-let venue = function(fs_data, fs_id) {
-
+let Venue = function(fs_data, fs_id) {
+    this.venueID = fs_data.id;
+    this.venueName = fs_data.name;
+    this.venueAddress = fs_data.location.formattedAddress;
+    this.marker = {};
+    this.lat = fs_data.location.lat;
+    this.lng = fs_data.location.lng;
+    this.venueFsUrl = "https://foursquare.com/v/" + this.venueID;
+    // this.venueCategoryName = fs_data.categories[0].name;
+    // this.venueCategoryShortName = fs_data.categories[0].shortName;
 }
 
 var placeModel = {
     self: this,
-
-    setBoundaries: function() {
-        "use strict";
-        if (markers && markers.length > 0) {
-            const numMarkers = markers.length;
-            // Extend the boundaries of the map for each marker and display the marker
-            for (let i = 0; i < numMarkers; i++) {
-                markers[i].setMap(map);
-                bounds.extend(markers[i].position);
-            }
-            map.fitBounds(bounds);
-        }
-    },
 
     // Creates a new marker icon with provided color.
     setMarkerIcon: function(color) {
@@ -40,62 +33,7 @@ var placeModel = {
             new google.maps.Point(10, 34),
             new google.maps.Size(21,34));
         return markerIcon;
-    },
-
-    //creates a marker for each location.
-    initMarkers: function () {
-        "use strict";
-
-        // Style the markers a bit. This will be our listing marker icon.
-        const defaultIcon = this.setMarkerIcon('0091ff');
-
-        // Create a "highlighted location" marker color for when the user
-        // mouses over the marker.
-        const highlightedIcon = this.setMarkerIcon('FFFF24');
-
-
-        if (this.all_locations && this.all_locations.length > 0) {
-            // Get the count of number of locations
-            const numLocations = this.all_locations.length;
-
-            for (var i = 0; i < numLocations; i++) {
-                let locationObj = this.all_locations[i];
-                const position = locationObj.location;
-                const title = locationObj.title;
-
-                // Create a marker per location, and put into markers array.
-                let marker = new google.maps.Marker({
-                    map: map,
-                    position: position,
-                    title: title,
-                    animation: google.maps.Animation.DROP,
-                    draggable: false,
-                    icon: defaultIcon,
-                    id: i
-                });
-
-                markers.push(marker);
-
-                // Create an onclick event to open the large infowindow at each marker.
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                });
-                // Two event listeners - one for mouseover, one for mouseout,
-                // to change the colors back and forth.
-                marker.addListener('mouseover', function() {
-                    this.setIcon(highlightedIcon);
-                });
-                marker.addListener('mouseout', function() {
-                    this.setIcon(defaultIcon);
-                });
-
-                locationObj.marker = marker;
-            }
-
-            this.setBoundaries();
-        }
-
-    }
+    }   
 
 }
 
@@ -105,11 +43,12 @@ var placeModel = {
 var ViewModel = function() {
 
     var self = this;
-    var venues = ko.observableArray(''),
+    var venues = ko.observableArray(),
         selectedVenue = ko.observable(''),
         venueMarkers = [],
         totalVenues = 0,
         searchStr = ko.observable('') // variable used for search/filter functionality
+        venue_marker_info_content = ''
     ;
 
     const FOURSQUARE_BASE_URL = 'https://api.foursquare.com/v2/venues/search?ll=';
@@ -120,10 +59,16 @@ var ViewModel = function() {
     // variable used to display Application Title
     self.appTitle = ko.observable("Neighborhood Insights");
 
+    function setMarkerInfoContent(venue) {
+
+    }
+
     function createMarker(venue) {
 
-        const venue_position = new google.maps.LatLng(venue.location.lat, venue.location.lng);
-
+        console.log('createMarker');
+        
+        const venue_position = new google.maps.LatLng(venue.lat, venue.lng);
+        
         var marker = new google.maps.Marker({
             map: map,
             position: venue_position,
@@ -133,9 +78,21 @@ var ViewModel = function() {
             // icon: defaultIcon,
         });
 
-        marker.addListener('click', function() {
+        
+        google.maps.event.addListener(marker, 'click', function() {
+            venue_marker_info_content = 'Venue Name: ' + venue.venueName + '\n';
+            infowindow.setContent(venue_marker_info_content);
             infowindow.open(map, marker);
         });
+
+        marker.setMap(map);
+
+        bounds = new google.maps.LatLngBounds();
+        bounds.extend(marker.position);
+
+        //map.fitBounds(bounds);
+
+        venue.marker = marker;
 
     }
 
@@ -153,16 +110,16 @@ var ViewModel = function() {
             cache: false,
             success: function (data) {
                 console.log(data.response.venues.length);
-                this.venues = data.response.venues;
-                //console.log(this.venues);
+                this.totalVenues = data.response.venues.length;
 
-                this.totalVenues = this.venues.length;
                 if (this.totalVenues > 0) {
-                    for(let i=0; i < 10; i++) {
-                        var venueObj = this.venues[i];
-                        // console.log( venueObj );
-                        createMarker(venueObj);
+                    for(let i = 0; i < 10; i++) {
+                        var item = data.response.venues[i];
+                        venues.push( new Venue(item) );
                     }
+                    venues().forEach(function(vitem) {
+                        createMarker(vitem);
+                    });
                 } else {
                     alert("No venues found for this location!!");
                 }
@@ -173,17 +130,6 @@ var ViewModel = function() {
     function getVenues(location) {
         console.log("getVenues");
         console.log(location);
-
-        this.venues = [];
-
-        infowindow = new google.maps.InfoWindow({
-            maxWidth: 150,
-            content: ""
-        });
-
-        map.addListener("click", function(){
-            infowindow.close(infowindow);
-        });
 
         // Get Nearby venues from this location
         getData(location);
@@ -205,12 +151,17 @@ function initMap() {
         console.log("google defined");
         mapOptions = {
             'center': start_point,
-            zoom: 17,
+            zoom: 16,
             disableDefaultUI: true
         };
 
         map = new google.maps.Map(document.getElementById('map'), mapOptions);
         $('#map').height($(window).height());
+
+        infowindow = new google.maps.InfoWindow({
+            maxWidth: 150,
+            content: ""
+        });
     }
 }
 
